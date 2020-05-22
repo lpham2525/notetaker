72bd74ef358d4a56d111619959aa5b9979093f9a
LanChi Pham <lpham2525@gmail.com>
2020-05-22 16:06:11 -0700

    code is now more concise, also added a gif and revised the README

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # Unit 11 Express: Note Taker
+----------------------------------------------------------------
+###  Description: Allows users to write, save, and delete notes.
+----------------------------------------------------------------
+#Demo: ![notetaker.gif](notetaker.gif)
+-------------------------------------------------------------
+#Table of Contents
+----------------------------------------------------------------
+ *Installation: npm i
 
-This application can be utilized to write and save notes as well as delete them. It uses express on the backend to store and retrieve notes from a file in JSON format. 
+ *Usage:The Note Taker application can be utilized to write and save notes as well as delete them. It uses express on the backend to store notes in string format. The app can also retrieve notes and displays them to the user in JSON format for enhanced readability. 
 
-The frontend had already been written so the project was aimed toward creating backend code and connecting it to the frontend.
+ *License: None
 
-For example:
+ *Test: node server.js or nodemon
+ ---------------------------------------------------------
+ 
+Which functions does the Note Taker have?
 
-  * GET `/notes` - Should return the `notes.html` file.
+* GET `/notes` - Should return the `notes.html` file.
 
   * GET `*` - Should return the `index.html` file
 
@@ -19,4 +30,7 @@ For example:
   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means that each note has a unique `id` when it's saved. In order to delete a note, the app reads all notes from the `db.json` file, removes the note with the given `id` property, and then rewrites the notes to the `db.json` file.
-
+---------------------------------------------------------
+#Made by LanChi Pham, &copy;Copyright 2020
+----------------------------------------------------------
+  
--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const app = express();
 const fs = require("fs");
 const { promisify } = require("util");
 const wf = promisify(fs.writeFile);
-const rf = promisify(fs.readFile);
-let idTracker = 0;
+const notesList = require("./db/db.json");
 
 app.use(express.static(join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -23,48 +22,28 @@ app.get("/notes", (req, res) => {
 
 //route to read the notes stored in the db.json file and display the notes in json format
 app.get("/api/notes", (req, res) => {
-  rf("./db/db.json", "utf-8")
-    .then((notesList) => {
-      notesList = JSON.parse(notesList);
-      res.json(notesList);
-    })
-    .catch((err) => console.error(err));
+  res.json(notesList);
 });
 
 //receives a new note to save on the request body, adds it to the`db.json` file, and then returns the new note to the client.
 app.post("/api/notes", (req, res) => {
   let { title, text } = req.body;
-  let newNote = { title, text, id: idTracker };
-  idTracker++;
-  rf("./db/db.json", "utf-8")
-    .then((notesList) => {
-      notesList = JSON.parse(notesList);
-      notesList.push(newNote);
-      wf("./db/db.json", JSON.stringify(notesList));
-      res.json(notesList);
-    })
-    .catch((err) => console.error(err));
+  let newNote = { title, text, id: `${notesList.length}` };
+  notesList.push(newNote);
+  wf("./db/db.json", JSON.stringify(notesList))
+    .then(() => res.send(notesList))
+    .catch(() => console.error(err));
 });
 
 //deletes selected note using the note's id, reassigns a new id to each of the notes
 app.delete("/api/notes/:id", (req, res) => {
-  let id = req.params.id;
-  console.log(id);
-  rf("./db/db.json", "utf-8")
-    .then((notesList) => {
-      notesList = JSON.parse(notesList);
-      notesList.splice(id, 1);
-      let reAssignID = (index) => {
-        for (let i = index; i < notesList.length; i++) {
-          notesList[i].id = i;
-        }
-      };
-      wf("./db/db.json", JSON.stringify(notesList))
-        .then(() => {
-          res.send(notesList);
-        })
-        .catch((err) => console.error(err));
-    })
+  notesList.splice(req.params.id, 1);
+  for (let newId = req.params.id; newId < notesList.length; newId++) {
+    notesList[newId].id = `${newId}`;
+  }
+
+  wf("./db/db.json", JSON.stringify(notesList))
+    .then(() => res.send(notesList))
     .catch((err) => console.error(err));
 });
 
