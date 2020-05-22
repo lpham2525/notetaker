250dd7d872c1baf816ee47028e78fff8f9c51c7d
LanChi Pham <lpham2525@gmail.com>
2020-05-21 21:16:09 -0700

    fixed several bugs in server.js

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# notetaker
+# Unit 11 Express: Note Taker
+
+This application can be utilized to write and save notes as well as delete them. It uses express on the backend to store and retrieve notes from a file in JSON format. 
+
+The frontend had already been written so the project was aimed toward creating backend code and connecting it to the frontend.
+
+For example:
+
+  * GET `/notes` - Should return the `notes.html` file.
+
+  * GET `*` - Should return the `index.html` file
+
+* The application has a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
+
+* The following API routes have been built:
+
+  * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+
+  * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
+  * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means that each note has a unique `id` when it's saved. In order to delete a note, the app reads all notes from the `db.json` file, removes the note with the given `id` property, and then rewrites the notes to the `db.json` file.
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const { join } = require('path')
+const app = express()
+const fs =require('fs')
+const { promisify } = require('util')
+const wf = promisify(fs.writeFile)
+const rf =promisify(fs.readFile)
+let idTracker = 0
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//HTML route to return the "index.html" file
+app.get('*'), (req, res) => {
+  res.sendFile(join(__dirname, "./public/index.html"))}
+
+//HTML route to return the "notes.html" file
+app.get('/notes'), (req, res)=> {
+  res.sendFile(join(__dirname, "./public/notes.html"))}
+
+//route to read the notes stored in the db.json file and display the notes in json format
+app.get('/api/notes', (req, res) => {
+  rf('./db/db.json', 'utf-8')
+    .then(notesList => {
+    notesList = JSON.parse(notesList)
+    res.json(notesList)
+    })
+    .catch(err => console.error(err))
+})
+
+//receives a new note to save on the request body, adds it to the`db.json` file, and then returns the new note to the client.
+app.post('/api/notes', (req, res) => {
+  let { title, text } = req.body
+  let newNote = { title, text, id: idTracker}
+  idTracker++
+  rf('./db/db.json', 'utf-8')
+  .then(notesList => {
+    notesList = JSON.parse(notesList)
+    notesList.push(newNote)
+    wf('./db/db.json', notesList)
+    res.json(notesList)
+  })
+  .catch(err => console.error(err))
+})
+  
+  //deletes selected note using the note's id, reassigns a new id to each of the notes
+app.delete('/api/notes/:id', (req, res) => {
+  let id = req.params.id
+  console.log(id)
+  rf('./db/db.json', 'utf-8')
+      .then(notesList =>{
+          notesList = JSON.parse(notesList)
+          notesList.splice(id, 1)
+          let reAssignID = (index) => {
+          for (let i = index; i < noteList.length; i++) {
+            noteList[i].id = i
+          }
+        }
+          wf('./db/db.json', notesList)
+            .then( ()=> {
+                res.send(notesList)
+            })
+            .catch(err => console.error(err))
+      })
+      .catch(err => console.error(err))
+})
+
+app.listen(3000, () => console.log('http://localhost:3000'))
